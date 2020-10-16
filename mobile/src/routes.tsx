fa1b5fcfd36f928eda684aa492183a0108e9731c
Marcus V. M. Martins <marcusmartins38@gmail.com>
2020-10-16 18:24:12 -0400

    Mobile: Component header

--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -1,20 +1,30 @@
-import React from 'react';
+import React from "react";
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import OrphanagesMap from './pages/OrphanagesMap';
-import OrphanageDetails from './pages/OrphanageDetails';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import OrphanagesMap from "./pages/OrphanagesMap";
+import OrphanageDetails from "./pages/OrphanageDetails";
+import SelectMapPosition from "./pages/CreateOrphanage/SelectMapPosition";
+import OrphanageData from "./pages/CreateOrphanage/OrphanageData";
 
 const { Navigator, Screen } = createStackNavigator();
 
 export default function Routes() {
   return (
     <NavigationContainer>
-      <Navigator screenOptions={{ headerShown: false}}>
-      <Screen name="OrphanagesMap" component={OrphanagesMap} />
+      <Navigator
+        screenOptions={{
+          headerShown: false,
+          cardStyle: { backgroundColor: "#f2f3f5" },
+        }}
+      >
+        <Screen name="OrphanagesMap" component={OrphanagesMap} />
 
-      <Screen name="OrphanageDetails" component={OrphanageDetails} />
+        <Screen name="OrphanageDetails" component={OrphanageDetails} />
+
+        <Screen name="SelectMapPosition" component={SelectMapPosition} />
+        <Screen name="OrphanageData" component={OrphanageData} />
       </Navigator>
     </NavigationContainer>
-  )
+  );
 }
